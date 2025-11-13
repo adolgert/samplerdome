86af5c9d94bb3fff481224ccb3c89b39550ba4cf
Andrew Dolgert <adolgert@andrew.cmu.edu>
2025-11-12 20:38:03 -0500

    added some includes and now it runs

--- src/Benchmarks.jl
+++ src/Benchmarks.jl
@@ -1,9 +1,36 @@
 module Benchmarks
 
+# Dependencies
+using Distributions
+using Random
+using DataStructures
+using BenchmarkTools
+
 export BenchmarkCondition, generate_conditions, is_compatible, construct_samplers
 export setup_sampler, benchmark_step!, benchmark_config, sampler_name
 export N_ENABLED, N_CHANGES, DISTRIBUTIONS, KEY_STRATEGIES
 
+# Include base types first
+include("base_types.jl")
+
+# Utility structures
+include("setofsets.jl")
+include("lefttrunc.jl")
+
+# Data structures for samplers
+include("binarytreeprefixsearch.jl")
+include("cumsumprefixsearch.jl")
+include("keyedprefixsearch.jl")
+
+# Sampler implementations
+include("direct.jl")
+include("firstreaction.jl")
+include("firsttofire.jl")
+include("combinednr.jl")
+include("pssa_cr.jl")
+include("rssa.jl")
+
+# Benchmarking framework
 include("conditions.jl")
 include("measure.jl")
 
